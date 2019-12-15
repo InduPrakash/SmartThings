58949fde54cc9425e16e77045fe89a5bb4642b8b
Indu Prakash <leolite1@gmail.com>
2019-12-15 06:38:21 -0600

    Updated icon

--- SmartApps/GarageDoorChecker.groovy
+++ SmartApps/GarageDoorChecker.groovy
@@ -19,8 +19,9 @@
 	author: "Indu Prakash",
 	description: "Sends notification if the garage door is open at the scheduled time.",
 	category: "My Apps",
-	iconUrl: "http://cdn.device-icons.smartthings.com/Transportation/transportation14-icn.png",
-	iconX2Url: "http://cdn.device-icons.smartthings.com/Transportation/transportation14-icn@2x.png") 
+	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Partner/doorbot.png",
+	iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Partner/doorbot@2x.png",
+	iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Partner/doorbot@2x.png")
 
 preferences {
 	input "door", "capability.garageDoorControl", title: "Which garage door to check?", required: true
